488f94a3968fb9c2daddfe3c738fc13685cc7d24
zhang96 <zhang96@users.noreply.github.com>
2016-02-14 11:29:35 -0500

    clean up
    Deleted unused js file
    Deleted some trash.
    Code cleanup done.
    Code minimization done.

--- js/typeaction.js
+++ js/typeaction.js
@@ -1,13 +1,8 @@
 $(function(){
-
     $("#typed").typed({
         strings: ["a web developer.", "an iOS developer.", "Fengwei Zhang."],
-        // stringsElement: $('#typed-strings'),
         typeSpeed: 30,
         backDelay: 500,
-        // loop: false,
-        // contentType: 'html',
-        loopCount: false,
         callback: function(){ foo(); },
         resetCallback: function() { newTyped(); }
     });
